c5d83b23e147af29f5161fd21c53a7c68f63b895
heidichen90 <toddypet@gmail.com>
2023-04-15 21:57:29 +0100

    create function createProduct

--- springboot-mall/src/main/java/com/example/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/example/springbootmall/dao/ProductDao.java
@@ -1,9 +1,11 @@
 package com.example.springbootmall.dao;
 
+import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
 
+    Integer createProduct(ProductRequest productRequest);
 }
