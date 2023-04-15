deb929de81af8d77ad03ce5562759bbecb4b968b
heidichen90 <toddypet@gmail.com>
2023-04-15 22:13:16 +0100

    create function updateProduct

--- springboot-mall/src/main/java/com/example/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/example/springbootmall/controller/ProductController.java
@@ -19,9 +19,9 @@ public class ProductController {
     @GetMapping("/products/{productId}")
     public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
         Product product = productService.getProductById(productId);
-        if(product != null){
+        if (product != null) {
             return ResponseEntity.status(HttpStatus.OK).body(product);
-        } else{
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
@@ -32,7 +32,22 @@ public class ProductController {
         Product product = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
-    };
+    }
 
+    ;
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+        // check if product exist
+        Product product = productService.getProductById(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        // update product
+        productService.updateProduct(productId, productRequest);
+        Product updatedProduct = productService.getProductById(productId);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+    }
 
 }
